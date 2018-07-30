08c007e2addf05d2003add10e837f391ee188690
Nattapon Nimakul <totizy@gmail.com>
2018-07-30 13:32:06 +0700

    add with ethereum

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import WithEthereum from './views/WithEthereum'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/WithEthereum',
+      name: 'WithEthereum',
+      component: WithEthereum
     },
     {
       path: '/about',
---
+++ src/views/WithEthereum.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="WithEthereum">
+    <img src="../assets/logo.png" class="logo">
+    <P2PFileSharingWithSmartContract/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import P2PFileSharingWithSmartContract from '@/components/P2PFileSharingWithSmartContract'
+
+export default {
+  name: 'WithEthereum',
+  components: {
+    P2PFileSharingWithSmartContract
+  }
+}
+</script>
+<style scoped>
+.logo {
+  max-width: 80%;
+}
+</style>
